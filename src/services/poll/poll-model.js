fd10041b615fc27c728e2c8e8c1af8e156709605
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-29 17:24:52 +0200

    generate poll service

---
+++ src/services/poll/poll-model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// poll-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const pollSchema = new Schema({
+  text: { type: String, required: true },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+
+const pollModel = mongoose.model('poll', pollSchema);
+
+module.exports = pollModel;
