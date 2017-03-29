9f6d960cb92c88c67f5566600003d809b2d60b79
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-29 17:28:55 +0200

    Add poll schema

--- src/services/poll/poll-model.js
+++ src/services/poll/poll-model.js
@@ -1,15 +1,28 @@
 'use strict';
 
 // poll-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const pollSchema = new Schema({
+const answerSchema = new Schema({
   text: { type: String, required: true },
+  voteCount: { type: Number, required: true, default: 0 }
+});
+
+const voterSchema = new Schema({
+  ip: { type: String },
+  sessionId: { type: String }
+});
+
+const pollSchema = new Schema({
+  createdBy: { type: Schema.ObjectId },
+  question: { type: String, required: true },
+  answers: [answerSchema],
+  voters: [voterSchema],
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
