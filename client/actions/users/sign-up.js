e800b5f2c4d4ff242b217892eb33dc2ec904b67a
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-30 12:57:04 +0200

    automatically sign in when signing up

--- client/actions/users/sign-up.js
+++ client/actions/users/sign-up.js
@@ -1,6 +1,7 @@
 // src/actions/users/sign-up.js
 import API from '../../middleware/api'
 import { USER_SIGNED_UP } from '../types'
+import signIn from './sign-in'
 
 const api = new API()
 const users = api.service('users')
@@ -10,6 +11,7 @@ export default (user) => {
     users.create(user)
     .then((res) => {
       dispatch({ type: USER_SIGNED_UP })
+      dispatch(signIn(user))
     })
     .catch((err) => {
       console.error(err)
