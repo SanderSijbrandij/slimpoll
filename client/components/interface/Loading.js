089b6f6b4fa4d6fe1608d73ff1621e892d44223a
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-30 16:00:33 +0200

    take out loading timer

--- client/components/interface/Loading.js
+++ client/components/interface/Loading.js
@@ -2,25 +2,17 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 
 class Loading extends PureComponent {
-  pastDelay() {
-    // return (new Date().getTime() - this.props.timer > 200)
-    return true
-  }
-
   render() {
     const { loading } = this.props
-    const classes = (loading && this.pastDelay()) ? 'loading' : 'notloading'
+    const classes = loading ? 'loading' : 'notloading'
 
     return (
       <div className={classes}>
-      { (!loading || !this.pastDelay()) && null }
-      { (loading && this.pastDelay()) && <span>Loading...</span> }
+      { !loading && null }
+      { loading && <span>Loading...</span> }
       </div>
     )
   }
 }
-const mapStateToProps = ({ loading }) => ({
-  loading: loading.status,
-  timer: loading.timer
-})
+const mapStateToProps = ({ loading }) => ({ loading })
 export default connect(mapStateToProps)(Loading)
--- client/reducers/loading.js
+++ client/reducers/loading.js
@@ -3,10 +3,10 @@ import { APP_LOADING, APP_READY } from '../actions/types'
 export default (state = false, { type }) => {
   switch(type) {
     case APP_LOADING:
-      return Object.assign({}, { status: true, timer: new Date().getTime()})
+      return true
 
     case APP_READY:
-      return Object.assign({}, { status: false, timer: new Date().getTime()})
+      return false
 
     default:
       return state
