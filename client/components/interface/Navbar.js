db6f71d87d82ecb36f3dcf4f8ff4734961a6f97e
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-30 11:18:35 +0200

    Add sign in/sign out to navbar

--- client/components/interface/Navbar.js
+++ client/components/interface/Navbar.js
@@ -1,7 +1,22 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
+import signOut from '../../actions/users/sign-out'
+
 class Navbar extends PureComponent {
+  checkLoginStatus() {
+    return !!this.props.currentUser ? this.renderSignOut() : this.renderSignIn()
+  }
+
+  renderSignOut() {
+    return <li onClick={this.props.signOut}><a>Sign Out</a></li>
+  }
+
+  renderSignIn() {
+    return <li><Link to='/sign-in'>Sign In</Link></li>
+  }
+
   render() {
     return(
       <header className='page-header'>
@@ -10,10 +25,11 @@ class Navbar extends PureComponent {
           <li><Link to="/all-polls">Polls</Link></li>
           <li><Link to="/create-poll">Create a poll</Link></li>
           <li><Link to="/my-polls">View your polls</Link></li>
+          { this.checkLoginStatus() }
         </nav>
       </header>
     )
   }
 }
-
-export default Navbar
+const mapStateToProps = ({ currentUser }) => ({ currentUser })
+export default connect(mapStateToProps, { signOut })(Navbar)
