60ecfea2f118d34845fa5ad20dc6e05c4fd9e0d3
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-04-03 08:45:30 +0200

    Add PropTypes to Error

--- client/components/interface/Error.js
+++ client/components/interface/Error.js
@@ -1,19 +1,23 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import clearErrors from '../../actions/interface/clear-errors'
 
 import closebutton from '../../assets/images/close.svg'
 
 class ErrorComp extends PureComponent {
+  static propTypes = {
+    error: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]).isRequired,
+    clearErrors: PropTypes.func.isRequired
+  }
+
   render() {
-    const { error } = this.props
+    const { error, clearErrors } = this.props
     if (!!error) {
       return (
         <div className='error'>
           <div className='closebutton'>
             <h3>{ error.type }</h3>
-            <img src={ closebutton } width='25px' height='25px'
-              onClick={ this.props.clearErrors } />
+            <img src={ closebutton } width='25px' height='25px' onClick={ clearErrors } />
           </div>
           <p>{ error.message }</p>
         </div>
