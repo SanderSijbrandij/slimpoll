7b9ed0e019f906c70183409c8ec0cdf1d40b4f40
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-04-07 14:06:37 +0200

    Add Pie and Bar chart components

---
+++ client/components/polls/BarChart.js
@@ -0,0 +1,10 @@
+import React, { PureComponent, PropTypes } from 'react'
+import * as d3 from 'd3'
+
+class BarChart extends PureComponent {
+  render() {
+    return null
+  }
+}
+
+export default BarChart
---
+++ client/components/polls/PieChart.js
@@ -0,0 +1,10 @@
+import React, { PureComponent, PropTypes } from 'react'
+import * as d3 from 'd3'
+
+class PieChart extends PureComponent {
+  render() {
+    return null
+  }
+}
+
+export default PieChart
