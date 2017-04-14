c2c9a232d30033989c20d8eeb36803f1702f8b29
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-04-14 12:49:04 +0200

    Make Pagination component

---
+++ client/components/interface/Pagination.js
@@ -0,0 +1,55 @@
+import React, { PureComponent, PropTypes } from 'react'
+import { Button, Icon } from 'semantic-ui-react'
+
+class Pagination extends PureComponent {
+  page(e, n) {
+    const { currentPage, pages } = this.props.data
+    if (n == 'prev') { 
+      n = (currentPage == 1) ? 1 : currentPage-1 
+    } else if (n == 'next') {
+      n = (currentPage == pages) ? pages : currentPage+1
+    }
+    this.props.fetchData(n)
+  }
+
+  makeButtonsArray() {
+    const { data } = this.props
+    
+    let buttons = []
+    for (let i = 1; i <= data.pages; i++) {
+      const button = data.currentPage == i
+      buttons.push(button)
+    }
+    return buttons
+  }
+
+  render() { 
+    const pages = this.makeButtonsArray()
+
+    return (
+      <div style={{ textAlign: 'center', margin: '0 auto' }}>
+      <Button.Group size='mini'>
+        <Button animated='fade' color='grey' onClick={ (e, n) => this.page(e, 'prev') }>
+          <Button.Content hidden><Icon name='chevron left' /></Button.Content>
+          <Button.Content visible>previous</Button.Content>
+        </Button>
+        {
+          pages.map((page, index) => {
+            return page ? 
+              <Button.Or key={index} text={ index + 1 } /> :
+              <Button key={index} onClick={ (e, n) => this.page(e, index+1) }>
+                { index + 1}
+              </Button>
+          })
+        }
+        <Button animated='fade' color='grey' onClick={ (e, n) => this.page(e, 'next') } >
+          <Button.Content hidden><Icon name='chevron right' /></Button.Content>
+          <Button.Content visible>next</Button.Content>
+        </Button>
+      </Button.Group>
+      </div>
+    )
+  }
+}
+
+export default Pagination
--- client/components/polls/PollsList.js
+++ client/components/polls/PollsList.js
@@ -5,60 +5,12 @@ import { List, Segment, Header, Divider, Button, Icon } from 'semantic-ui-react'
 import { history } from '../../store'
 import { getTotalVotes } from '../../helpers/votes'
 import PollItem from './PollItem'
+import Pagination from '../interface/Pagination'
 import fetchPolls from '../../actions/polls/fetch'
 
 class PollsList extends PureComponent {
   componentDidMount() {
     if (!this.props.currentUser) { history.push('/sign-in') }
-  }
-
-  page(e, n) {
-    const { currentPage, pages } = this.props.pollsData
-    if (n == 'prev') { 
-      n = (currentPage == 1) ? 1 : currentPage-1 
-    } else if (n == 'next') {
-      n = (currentPage == pages) ? pages : currentPage+1
-    }
-    this.props.fetchPolls(n)
-  }
-
-  makeButtonsArray() {
-    const { pollsData } = this.props
-    
-    let buttons = []
-    for (let i = 1; i <= pollsData.pages; i++) {
-      const button = pollsData.currentPage == i
-      buttons.push(button)
-    }
-    return buttons
-  }
-
-  renderPagination() {
-    const pages = this.makeButtonsArray()
-
-    return (
-      <div style={{ textAlign: 'center', margin: '0 auto' }}>
-      <Button.Group size='mini'>
-        <Button animated='fade' color='grey' onClick={ (e, n) => this.page(e, 'prev') }>
-          <Button.Content hidden><Icon name='chevron left' /></Button.Content>
-          <Button.Content visible>previous</Button.Content>
-        </Button>
-        {
-          pages.map((page, index) => {
-            return page ? 
-              <Button.Or key={index} text={ index + 1 } /> :
-              <Button key={index} onClick={ (e, n) => this.page(e, index+1) }>
-                { index + 1}
-              </Button>
-          })
-        }
-        <Button animated='fade' color='grey' onClick={ (e, n) => this.page(e, 'next') } >
-          <Button.Content hidden><Icon name='chevron right' /></Button.Content>
-          <Button.Content visible>next</Button.Content>
-        </Button>
-      </Button.Group>
-      </div>
-    )
   }
 
   renderPoll(p, i) {
@@ -77,14 +29,14 @@ class PollsList extends PureComponent {
           { allPolls ? 'All Polls' : 'Your Polls' }
         </Header>
         <Segment attached>
-          { this.renderPagination() }
+          <Pagination data={this.props.pollsData} fetchData={this.props.fetchPolls} />
           <Divider />
           <List animated divided relaxed link>
             { polls.map((p, i) => this.renderPoll(p, i)) }
           </List>
         </Segment>
         <Segment attached>
-          { this.renderPagination() }
+          <Pagination data={this.props.pollsData} fetchData={this.props.fetchPolls} />
         </Segment>
       </Segment>
     )
